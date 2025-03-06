0ea5afe89e051523ad00c085f4ec23be9c3e4a3d
Dogukan Calkus <dogukancalkusb@gmail.com>
2025-03-06 07:41:43 +0300

    program.cs eklendi

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cba626dd86c837a17c8506e0c27026a4864a8d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f90e075ff167da2067905c0de81ca216e1fca63")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
